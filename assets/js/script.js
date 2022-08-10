44a1281afd1eb39bf0758d0f773771a1d1a80de2
Michaela Varberg <michaelavarberg@gmail.com>
2022-08-10 12:40:48 -0600

    created html elements and started styling in css

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+//variable declarations(array of question objects (with options, correct vs. incorrect))
+var question1 = {
+    question: "",
+    option1: "",
+    option2: "",
+    option3: "",
+    option4: "",
+    correct: question1.option1
+}
+var question2 = {
+    question: "",
+    option1: "",
+    option2: "",
+    option3: "",
+    option4: "",
+    correct: question2.option1
+}
+var question3 = {
+    question: "",
+    option1: "",
+    option2: "",
+    option3: "",
+    option4: "",
+    correct: question3.option1
+}
+var question4 = {
+    question: "",
+    option1: "",
+    option2: "",
+    option3: "",
+    option4: "",
+    correct: question4.option1
+}
+var question5 = {
+    question: "",
+    option1: "",
+    option2: "",
+    option3: "",
+    option4: "",
+    correct: question5.option1
+}
+var questionsArray = [question1, question2, question3, question4, question5];
+
+//functions
+function setTimer();
+
+function newQuestion();
+
+function gameOver();
+
+//event handlers
+
+//clicks start
+
+//answers question
+
+//saves score at the end
+
+//clicks view high score (optional)
